881998cb7e01bcbae55c6427ce34d3d05e46da47
DueWeb <keviin.sundberg@gmail.com>
2023-04-25 15:39:55 +0200

    tryning fixes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-// Import Express, a web server framework
-const express = require('express');
-// Create a webserver
-const app = express();
-// Serve the content in the lumiashop folder
-app.use(express.static('lumiashop'));
-// Start the webserver
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+const express = require('express')
+const app = express()
+const port = 5500
+// const path = require('path');
+
+app.use(express.static('lumia-shop'))
+
+app.listen(port, () => {
+    console.log(`Test app listening on port ${port}`)
+})
